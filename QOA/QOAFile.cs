e54fb2f37e5d7fa09ad33a0e0ec2866e1188c59e
TollyH <ptolemyhill@sky.com>
2025-01-18 22:52:01 +0000

    Add QOAFrame class

--- QOA/QOAFile.cs
+++ QOA/QOAFile.cs
@@ -1,11 +1,36 @@
 ﻿namespace QOA
 {
-    public class QOAFile
+    public class QOAFrame
     {
         public const int SlicesPerFrame = 256;
         public const int SamplesPerSlice = 20;
         public const int SamplesPerFrame = SlicesPerFrame * SamplesPerSlice;
 
+        public byte ChannelCount { get; }
+        public uint SampleRate { get; }
+        public uint SamplesPerChannel { get; }
+        public ushort Size { get; }
+
+        public short[][] ChannelSamples { get; }
+
+        public QOAFrame(byte channelCount, uint sampleRate, ushort samplesPerChannel, ushort size)
+        {
+            ChannelCount = channelCount;
+            SampleRate = sampleRate;
+            SamplesPerChannel = samplesPerChannel;
+            Size = size;
+
+            ChannelSamples = new short[ChannelCount][];
+
+            for (int i = 0; i < ChannelCount; i++)
+            {
+                ChannelSamples[i] = new short[SamplesPerChannel];
+            }
+        }
+    }
+
+    public class QOAFile
+    {
         public static readonly byte[] MagicBytes = new byte[4] { 113, 111, 97, 102 };  // 'qoaf'
 
         public byte ChannelCount { get; }
@@ -24,11 +49,11 @@
 
             TrailingData = trailingData;
 
-            ChannelSamples = new short[SamplesPerFrame][];
+            ChannelSamples = new short[ChannelCount][];
 
-            for (int i = 0; i < SamplesPerFrame; i++)
+            for (int i = 0; i < ChannelCount; i++)
             {
-                ChannelSamples[i] = new short[ChannelCount];
+                ChannelSamples[i] = new short[SamplesPerChannel];
             }
         }
     }
